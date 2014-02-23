929e9d3ab83862f7560f8a742372ada7c01cbe6f
Gerald Schreiber <schreibergerald1@gmail.com>
2014-02-23 10:25:18 +0100

    added classes to build json objects

---
+++ source/big-buum-man-server/src/at/big_buum_man/server/network/JSONNetworkObject.java
@@ -0,0 +1,50 @@
+/*
+ * Gerald Schreiber
+ * <schreibergerald1@gmail.com>
+ */
+
+package at.big_buum_man.server.network;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+public class JSONNetworkObject {
+
+	private final String JSON_RPC_VERSION = "2.0";
+
+	private String method = "";
+	private HashMap<String, Object> params = new HashMap<String, Object>();
+
+	public String getMethod() {
+		return method;
+	}
+	
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	
+	public HashMap<String, Object> getParams() {
+		return params;
+	}
+	
+	public void setParams(HashMap<String, Object> params) {
+		this.params = params;
+	}
+	
+	public void addParam(String key, Object value) {
+		params.put(key, value);
+	}
+	
+	public void removeParam(String key) {
+		params.remove(key);
+	}
+	
+	public JSONObject getJSONObject() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("jsonrpc", JSON_RPC_VERSION);
+		jsonObject.put("method", method);
+		jsonObject.put("params", params);
+		return jsonObject;
+	}
+}

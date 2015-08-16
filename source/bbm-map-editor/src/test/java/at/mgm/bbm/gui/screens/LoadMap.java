35b5d92c90ecd28d4eddd1d805c5c37ed272ea55
Michael Palata <michael.palata@gmail.com>
2015-08-16 19:00:39 +0200

    Updated LoadMapScreen and refactored Screen Entries

--- source/bbm-map-editor/src/test/java/at/mgm/bbm/gui/screens/LoadMap.java
+++ source/bbm-map-editor/src/test/java/at/mgm/bbm/gui/screens/LoadMap.java
@@ -4,7 +4,7 @@ import at.mgm.bbm.core.FileUtils;
 import at.mgm.bbm.core.States;
 import at.mgm.bbm.core.map.Map;
 import at.mgm.bbm.gui.Resources;
-import at.mgm.bbm.gui.entries.ListEntry;
+import at.mgm.bbm.gui.entries.Entry;
 import org.newdawn.slick.*;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.BasicGameState;
@@ -16,11 +16,20 @@ import java.util.List;
 public class LoadMap extends BasicGameState {
 
     private final String TITLE = "Choose a map";
+    private final String BACK = "back";
+
+    private int TITLE_X = 0;
+
+    private int BACK_X = 0;
+    private int BACK_Y = 0;
+
     private StateBasedGame game;
 
-    private List<ListEntry> entries = new ArrayList<ListEntry>();
+    private List<Entry> entries = new ArrayList<Entry>();
     private Font font;
     private Font font2;
+
+    private Entry back;
 
     @Override
     public int getID() {
@@ -36,7 +45,7 @@ public class LoadMap extends BasicGameState {
         int x = 100;
 
         for (int i = 0; i < files.length; i++) {
-            entries.add(new ListEntry(x, 300 + (20 * i), font.getWidth(files[i].getName()), font.getHeight(files[i].getName()), i, files[i]));
+            entries.add(new Entry(i, x, 300 + (20 * i), font.getWidth(files[i].getName()), font.getHeight(files[i].getName()), files[i]));
         }
     }
 
@@ -46,14 +55,22 @@ public class LoadMap extends BasicGameState {
 
         font  = paramGameContainer.getDefaultFont();
         font2 = Resources.INSTANCE.font;
+
+        TITLE_X = font2.getWidth(TITLE) / 2;
+
+        BACK_X = font2.getWidth(BACK);
+        BACK_Y = font2.getHeight(BACK);
+
+        back = new Entry(States.ACTION_EXIT, 960 - (BACK_X / 2), 950, BACK_X, BACK_Y, null);
     }
 
     @Override
     public void render(final GameContainer paramGameContainer, final StateBasedGame paramStateBasedGame, final Graphics paramGraphics) throws SlickException {
-        font2.drawString(960 - (font2.getWidth(TITLE) / 2), 100, TITLE);
-        for (ListEntry entry : entries) {
+        font2.drawString(960 - TITLE_X, 100, TITLE);
+        for (Entry entry : entries) {
             font.drawString(entry.x, entry.y, entry.text);
         }
+        font2.drawString(960 - (BACK_X / 2), 950, BACK);
     }
 
     @Override
@@ -64,13 +81,16 @@ public class LoadMap extends BasicGameState {
         int mouseY = input.getMouseY();
 
         if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-            for (ListEntry entry : entries) {
+            for (Entry entry : entries) {
                 if (entry.checkCollision(mouseX, mouseY)) {
                     System.out.println(entry.text);
                     File map = (File) entry.getObject();
                     Map.INSTANCE.loadMap(map, 0, 0);
                     game.enterState(States.SCREEN_EDITOR);
                 }
+            }
+            if (back.checkCollision(mouseX, mouseY)) {
+                game.enterState(States.SCREEN_MENU);
             }
         } else if (input.isKeyPressed(Input.KEY_ESCAPE)) {
             game.enterState(States.SCREEN_MENU);
